dfdfd5d8cc3d3182d54777b991c630e19ba1e715
Uli <SirUli@users.noreply.github.com>
2023-07-20 21:38:12 +0200

    fix: Amend script to allow for single-phase meters

--- fronius-smartmeter.py
+++ fronius-smartmeter.py
@@ -15,6 +15,7 @@ Reading information from the Fronius Smart Meter via http REST API and puts the 
 """
 
 import contextlib
+
 try:
     import gobject
 except ImportError:
@@ -35,8 +36,10 @@ except ImportError:
     import _thread as thread  # for daemon = True  / Python 3.x
 
 # use an established Victron service to maintain compatiblity
-sys.path.insert(1, os.path.join('/opt/victronenergy/dbus-systemcalc-py', 'ext', 'velib_python'))
-from vedbus import VeDbusService, VeDbusItemImport
+sys.path.insert(
+    1, os.path.join("/opt/victronenergy/dbus-systemcalc-py", "ext", "velib_python")
+)
+from vedbus import VeDbusService, VeDbusItemImport  # noqa E402
 
 log = logging.getLogger("DbusFroniusSmartMeter")
 path_UpdateIndex = "/UpdateIndex"
@@ -55,7 +58,6 @@ class DbusFroniusService:
         return val[0], int(val[1])
 
     def detect(self):
-
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -145,13 +147,13 @@ class DbusFroniusService:
             "/Role", self.role, writeable=True, onchangecallback=self.role_changed
         )
 
-        _kwh = lambda p, v: (str(round(v, 2) or '') + "kWh")
-        _a = lambda p, v: (str(round(v, 1) or '') + "A")
-        _w = lambda p, v: (str(round(v, 1) or '') + "W")
-        _v = lambda p, v: (str(round(v, 1) or '') + "V")
-        _ms = lambda p, v: (str(v or '') + "ms")
-        _hz = lambda p, v: (str(v or '') + "Hz")
-        _x = lambda p, v: (str(v or ''))
+        _kwh = lambda p, v: (str(round(v, 2) or "") + "kWh")  # noqa E731
+        _a = lambda p, v: (str(round(v, 1) or "") + "A")  # noqa E731
+        _w = lambda p, v: (str(round(v, 1) or "") + "W")  # noqa E731
+        _v = lambda p, v: (str(round(v, 1) or "") + "V")  # noqa E731
+        _ms = lambda p, v: (str(v or "") + "ms")  # noqa E731
+        _hz = lambda p, v: (str(v or "") + "Hz")  # noqa E731
+        _x = lambda p, v: (str(v or ""))  # noqa E731
 
         self._paths = {
             "/Ac/Power": {"initial": 0, "textformat": _w},
@@ -241,47 +243,12 @@ class DbusFroniusService:
             self._dbusservice["/Ac/L2/Energy/Reverse"] = None
             self._dbusservice["/Ac/L3/Energy/Forward"] = None
             self._dbusservice["/Ac/L3/Energy/Reverse"] = None
-        if meter_model == 'Smart Meter TS 100A-1' or meter_model == "Smart Meter 63A-1":  # set values for single phase meter
-            meter_data["Voltage_AC_Phase_2"] = 0
-            meter_data["Voltage_AC_Phase_3"] = 0
-            meter_data["Current_AC_Phase_2"] = 0
-            meter_data["Current_AC_Phase_3"] = 0
-            meter_data["PowerReal_P_Phase_2"] = 0
-            meter_data["PowerReal_P_Phase_3"] = 0
-            self._dbusservice["/Ac/L2/Energy/Forward"] = None
-            self._dbusservice["/Ac/L2/Energy/Reverse"] = None
-            self._dbusservice["/Ac/L3/Energy/Forward"] = None
-            self._dbusservice["/Ac/L3/Energy/Reverse"] = None
-        if "Frequency_Phase_Average" in meter_data:    
-            self._dbusservice["/Ac/Frequency"] = meter_data["Frequency_Phase_Average"]
         if "Voltage_AC_Phase_1" in meter_data:
             self._dbusservice["/Ac/L1/Voltage"] = meter_data["Voltage_AC_Phase_1"]
-        if "Voltage_AC_Phase_2" in meter_data:
-            self._dbusservice["/Ac/L2/Voltage"] = meter_data["Voltage_AC_Phase_2"]
-        if "Voltage_AC_Phase_3" in meter_data:
-            self._dbusservice["/Ac/L3/Voltage"] = meter_data["Voltage_AC_Phase_3"]
-        if "Current_AC_Sum" in meter_data:
-            self._dbusservice["/Ac/Current"] = meter_data["Current_AC_Sum"]
         if "Current_AC_Phase_1" in meter_data:
             self._dbusservice["/Ac/L1/Current"] = meter_data["Current_AC_Phase_1"]
-        if "Current_AC_Phase_2" in meter_data:
-            self._dbusservice["/Ac/L2/Current"] = meter_data["Current_AC_Phase_2"]
-        if "Current_AC_Phase_3" in meter_data:
-            self._dbusservice["/Ac/L3/Current"] = meter_data["Current_AC_Phase_3"]
-        # positive: consumption, negative: feed into grid
-        self._dbusservice["/Ac/Power"] = meter_consumption
         if "PowerReal_P_Phase_1" in meter_data:
             self._dbusservice["/Ac/L1/Power"] = meter_data["PowerReal_P_Phase_1"]
-        if "PowerReal_P_Phase_2" in meter_data:
-            self._dbusservice["/Ac/L2/Power"] = meter_data["PowerReal_P_Phase_2"]
-        if "PowerReal_P_Phase_3" in meter_data:
-            self._dbusservice["/Ac/L3/Power"] = meter_data["PowerReal_P_Phase_3"]
-        self._dbusservice["/Ac/Energy/Forward"] = (
-            float(meter_data["EnergyReal_WAC_Sum_Consumed"]) / 1000.0
-        )
-        self._dbusservice["/Ac/Energy/Reverse"] = (
-            float(meter_data["EnergyReal_WAC_Sum_Produced"]) / 1000.0
-        )
         if "EnergyReal_WAC_Phase_1_Consumed" in meter_data:
             self._dbusservice["/Ac/L1/Energy/Forward"] = (
                 float(meter_data["EnergyReal_WAC_Phase_1_Consumed"]) / 1000
@@ -290,22 +257,48 @@ class DbusFroniusService:
             self._dbusservice["/Ac/L1/Energy/Reverse"] = (
                 float(meter_data["EnergyReal_WAC_Phase_1_Produced"]) / 1000
             )
-        if "EnergyReal_WAC_Phase_2_Consumed" in meter_data:
-            self._dbusservice["/Ac/L2/Energy/Forward"] = (
-                float(meter_data["EnergyReal_WAC_Phase_2_Consumed"]) / 1000
-            )
-        if "EnergyReal_WAC_Phase_2_Produced" in meter_data:
-            self._dbusservice["/Ac/L2/Energy/Reverse"] = (
-                float(meter_data["EnergyReal_WAC_Phase_2_Produced"]) / 1000
-            )
-        if "EnergyReal_WAC_Phase_3_Consumed" in meter_data:
-            self._dbusservice["/Ac/L3/Energy/Forward"] = (
-                float(meter_data["EnergyReal_WAC_Phase_3_Consumed"]) / 1000
-            )
-        if "EnergyReal_WAC_Phase_3_Produced" in meter_data:
-            self._dbusservice["/Ac/L3/Energy/Reverse"] = (
-                float(meter_data["EnergyReal_WAC_Phase_3_Produced"]) / 1000
-            )
+        # Only set three phase values if the meter is not a single-phase meter
+        if meter_model not in ("Smart Meter TS 100A-1", "Smart Meter 63A-1"):
+            if "Voltage_AC_Phase_2" in meter_data:
+                self._dbusservice["/Ac/L2/Voltage"] = meter_data["Voltage_AC_Phase_2"]
+            if "Voltage_AC_Phase_3" in meter_data:
+                self._dbusservice["/Ac/L3/Voltage"] = meter_data["Voltage_AC_Phase_3"]
+            if "Current_AC_Phase_2" in meter_data:
+                self._dbusservice["/Ac/L2/Current"] = meter_data["Current_AC_Phase_2"]
+            if "Current_AC_Phase_3" in meter_data:
+                self._dbusservice["/Ac/L3/Current"] = meter_data["Current_AC_Phase_3"]
+            if "PowerReal_P_Phase_2" in meter_data:
+                self._dbusservice["/Ac/L2/Power"] = meter_data["PowerReal_P_Phase_2"]
+            if "PowerReal_P_Phase_3" in meter_data:
+                self._dbusservice["/Ac/L3/Power"] = meter_data["PowerReal_P_Phase_3"]
+            if "EnergyReal_WAC_Phase_2_Consumed" in meter_data:
+                self._dbusservice["/Ac/L2/Energy/Forward"] = (
+                    float(meter_data["EnergyReal_WAC_Phase_2_Consumed"]) / 1000
+                )
+            if "EnergyReal_WAC_Phase_2_Produced" in meter_data:
+                self._dbusservice["/Ac/L2/Energy/Reverse"] = (
+                    float(meter_data["EnergyReal_WAC_Phase_2_Produced"]) / 1000
+                )
+            if "EnergyReal_WAC_Phase_3_Consumed" in meter_data:
+                self._dbusservice["/Ac/L3/Energy/Forward"] = (
+                    float(meter_data["EnergyReal_WAC_Phase_3_Consumed"]) / 1000
+                )
+            if "EnergyReal_WAC_Phase_3_Produced" in meter_data:
+                self._dbusservice["/Ac/L3/Energy/Reverse"] = (
+                    float(meter_data["EnergyReal_WAC_Phase_3_Produced"]) / 1000
+                )
+        if "Frequency_Phase_Average" in meter_data:
+            self._dbusservice["/Ac/Frequency"] = meter_data["Frequency_Phase_Average"]
+        if "Current_AC_Sum" in meter_data:
+            self._dbusservice["/Ac/Current"] = meter_data["Current_AC_Sum"]
+        # positive: consumption, negative: feed into grid
+        self._dbusservice["/Ac/Power"] = meter_consumption
+        self._dbusservice["/Ac/Energy/Forward"] = (
+            float(meter_data["EnergyReal_WAC_Sum_Consumed"]) / 1000.0
+        )
+        self._dbusservice["/Ac/Energy/Reverse"] = (
+            float(meter_data["EnergyReal_WAC_Sum_Produced"]) / 1000.0
+        )
         self._dbusservice["/Latency"] = self._latency
         log.info(
             "Meter Power: %s, Latency: %.1fms"
@@ -358,7 +351,7 @@ def main():
     # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
     DBusGMainLoop(set_as_default=True)
 
-    pvac_output = DbusFroniusService(
+    DbusFroniusService(
         servicename="com.victronenergy.grid.fronius", deviceinstance=40, ip=args.ip
     )
 
